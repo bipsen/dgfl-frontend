89faf2b73329a0c13a365b86eaa252b41e78c7a6
bipsen <ipsen.bertil@gmail.com>
2023-02-04 11:12:31 +0100

    added login nav guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,14 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
+import { getCurrentUser } from 'vuefire'
 
 const routes = [
   {
     path: '/',
     component: () => import('@/layouts/default/Default.vue'),
+    meta: {
+      requiresAuth: true
+    },
     children: [
       {
         path: 'home',
@@ -69,5 +73,24 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 })
+
+router.beforeEach(async (to) => {
+  // routes with `meta: { requiresAuth: true }` will check for the users, others won't
+  if (to.meta.requiresAuth) {
+    const currentUser = await getCurrentUser()
+    // if the user is not logged in, redirect to the login page
+    if (!currentUser) {
+      return {
+        path: '/login',
+        query: {
+          // we keep the current path in the query so we can redirect to it after login
+          // with `router.push(route.query.redirectTo || '/')`
+          redirectTo: to.fullPath,
+        },
+      }
+    }
+  }
+})
+
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <h1>Home</h1>
+  <p v-if="user">Hello {{ user.providerData[0].email }}</p>
 </template>
 
 <script lang="ts" setup>
+import { useCurrentUser } from 'vuefire'
 
+const user = useCurrentUser()
 </script>
