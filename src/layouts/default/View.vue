28517ab5aeb1468862d43ab3a74aacb66ac5aa63
bipsen <ipsen.bertil@gmail.com>
2023-02-09 14:35:22 +0100

    Added fade transitions

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,9 +1,26 @@
 <template>
   <v-main>
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </v-main>
 </template>
+
 
 <script lang="ts" setup>
   //
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
