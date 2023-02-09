06b85cbc5d95b9a92c73e4f64bfac1e71f996c6a
bipsen <ipsen.bertil@gmail.com>
2023-02-09 15:07:42 +0100

    Removed buggy transitions

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,9 +1,6 @@
 <template>
   <v-main>
-    <router-view v-slot="{ Component }">
-      <transition name="fade" mode="out-in">
-        <component :is="Component" />
-      </transition>
+    <router-view>
     </router-view>
   </v-main>
 </template>
@@ -11,16 +8,3 @@
 
 <script lang="ts" setup>
   //
-</script>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.2s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
