b650cd35f2c357612db9975563cd4485fc6068ab
bipsen <ipsen.bertil@gmail.com>
2023-02-10 10:25:15 +0100

    Fixes to sign up system

--- src/views/SignUpScreen.vue
+++ src/views/SignUpScreen.vue
@@ -1,39 +1,33 @@
 <template>
     <v-sheet class="pa-12" rounded>
         <v-card class="mx-auto px-6 py-8" rounded="lg">
+            <form @submit.prevent="submit">
 
-            <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+                <v-text-field v-model="name.value.value" :error-messages="name.errorMessage.value" density="compact"
+                    placeholder="Name" prepend-inner-icon="mdi-account" variant="outlined" />
 
-            <v-text-field v-model="name" density="compact" placeholder="Name" prepend-inner-icon="mdi-account"
-                variant="outlined" />
+                <v-text-field v-model="email.value.value" :error-messages="email.errorMessage.value" density="compact"
+                    placeholder="Email address" prepend-inner-icon="mdi-email-outline" variant="outlined" />
 
-            <v-text-field v-model="email" density="compact" placeholder="Email address"
-                prepend-inner-icon="mdi-email-outline" variant="outlined" />
+                <v-text-field v-model="password.value.value" :counter="10" :error-messages="password.errorMessage.value"
+                    :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
+                    density="compact" placeholder="Enter a password" prepend-inner-icon="mdi-lock-outline"
+                    variant="outlined" @click:append-inner="visible = !visible" />
 
-            <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-                Password
-            </div>
+                <v-select v-model="team.value.value" :error-messages="team.errorMessage.value" :items="teams"
+                    variant="underlined" density="compact" />
 
-            <v-text-field v-model="password" :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-                :type="visible ? 'text' : 'password'" density="compact" placeholder="Enter a password"
-                prepend-inner-icon="mdi-lock-outline" variant="outlined" @click:append-inner="visible = !visible" />
+                <v-btn type="submit" block class="mb-8" size="large" variant="tonal">
+                    Sign Up
+                </v-btn>
 
-            <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-                Team
-            </div>
-
-            <v-select v-model="team" :items="teams" variant="underlined" density="compact" />
-
-            <v-btn block class="mb-8" size="large" variant="tonal" @click="onSignUp">
-                Sign Up
-            </v-btn>
-
-            <v-card-text class="text-center">
-                <a class="text-blue text-decoration-none" href="/login" rel="noopener noreferrer">
-                    I already have an account
-                    <v-icon icon="mdi-chevron-right"></v-icon>
-                </a>
-            </v-card-text>
+                <v-card-text class="text-center">
+                    <a class="text-blue text-decoration-none" href="/login" rel="noopener noreferrer">
+                        I already have an account
+                        <v-icon icon="mdi-chevron-right"></v-icon>
+                    </a>
+                </v-card-text>
+            </form>
         </v-card>
     </v-sheet>
 </template>
@@ -45,6 +39,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { collection, doc, setDoc } from "firebase/firestore";
 import { useRouter } from 'vue-router'
 import { useAppStore } from '@/store/app'
+import { useField, useForm } from 'vee-validate'
 
 const appStore = useAppStore()
 
@@ -54,9 +49,8 @@ const auth = useFirebaseAuth()
 
 const form = ref(false)
 const visible = ref(false)
-const name = ref(null)
-const email = ref(null)
-const password = ref(null)
+
+
 const loading = ref(false)
 const players = useCollection(collection(db, 'players'))
 const teams = computed(() => {
@@ -65,14 +59,42 @@ const teams = computed(() => {
         return acc;
     }, new Set);
 })
-const team = ref(teams.value[0])
 
-function onSignUp() {
+const { handleSubmit, handleReset } = useForm({
+    validationSchema: {
+        name(value: string) {
+            if (value?.length >= 2) return true
+
+            return 'Name needs to be at least 2 characters.'
+        },
+        email(value: string) {
+            if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
+
+            return 'Must be a valid e-mail.'
+        },
+        password(value: string) {
+            if (value?.length >= 6) return true
+
+            return 'Password needs to be at least 6 characters.'
+        },
+        team(value: string) {
+            if (value) return true
+            return 'Select a team.'
+        },
+    },
+})
+
+const name = useField('name')
+const email = useField('email')
+const password = useField('password')
+const team = useField('team')
+
+const submit = handleSubmit(values => {
     loading.value = true
-    createUserWithEmailAndPassword(auth!, email.value!, password.value!)
+    createUserWithEmailAndPassword(auth!, values.email, values.password)
         .then((userCredential) => {
             const user = userCredential.user
-            createUserEntry(user.uid)
+            createUserEntry(user.uid, values)
             loading.value = false
             router.push('/')
         })
@@ -81,15 +103,15 @@ function onSignUp() {
             const errorMessage = error.message;
             loading.value = false
         });
-}
+})
 
-async function createUserEntry(uid: string) {
+async function createUserEntry(uid: string, userInfo: Object) {
     // Add a new document in collection "cities"
     await setDoc(doc(db, "users", uid), {
         cash: 1000000,
         roster: [],
-        name: name.value,
-        team: team.value
+        name: userInfo.name,
+        team: userInfo.team
     });
 }
 
